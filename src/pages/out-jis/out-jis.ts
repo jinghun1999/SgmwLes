489eb5e9f533d5f892acbf35f0973a6ae590dcce
YuanZiFeng <yuan_zifeng@qq.com>
2019-02-28 20:13:52 +0800

    Merge branch 'master' of https://github.com/jinghun1999/SgmwLes
    # Conflicts:
    #	src/pages/jisOutStock/jisOutStock.ts

--- src/pages/out-jis/out-jis.ts
+++ src/pages/out-jis/out-jis.ts
@@ -10,6 +10,7 @@ import {
 import {BaseUI} from '../baseUI';
 import {Api} from '../../providers';
 import {Storage} from "@ionic/storage";
+import {INT_TYPE} from "@angular/compiler/src/output/output_ast";
 
 @IonicPage()
 @Component({
@@ -111,51 +112,79 @@ export class OutJisPage extends BaseUI {
 
     let i = this.item.parts.findIndex(p => p.part_no === _part_num && p.supplier_id === _supplier_number);
     if (i >= 0) {
-      super.showToast(this.toastCtrl, '该标签已扫描，不能重复添加');
+
+      let tmpPart=this.item.parts[i];
+      let requireBoxes=tmpPart.require_boxes+1;
+      let requireParts=tmpPart.require_parts+tmpPart.std_qty;
+
+      if(requireBoxes>tmpPart.current_boxes){
+        super.showToast(this.toastCtrl, '零件达到最大箱数，不能继续扫箱！');
+        this.refreshDataModal();
+        return;
+      }
+      if(requireParts>tmpPart.current_parts){
+        super.showToast(this.toastCtrl, '零件达到最大件数，不能继续扫箱！');
+        this.refreshDataModal();
+        return;
+      }
+      tmpPart.require_boxes=requireBoxes;
+      tmpPart.require_parts=requireParts;
+
       this.label = '';
       this.searchbar.setFocus();
       return;
     }
-    //合法标签
-    let loading = super.showLoading(this.loadingCtrl, '加载中...');
-    this.api.get('wm/getPartByLN', {
-      plant: this.item.plant,
-      workshop: this.item.workshop,
-      ln: this.label
-    }).subscribe((res: any) => {
-        if (res.successful) {
-          let pts = res.data;
-          if (pts.length > 0) {
-            this.item.parts.push({
-              plant: pts[0].plant,
-              workshop: pts[0].workshop,
-              part_no: pts[0].part_no,
-              part_name: pts[0].part_name,
-              supplier_id: pts[0].supplier_id,
-              supplier_name: pts[0].supplier_name,
-              dloc: pts[0].dloc,
-              unit: pts[0].unit,
-              std_qty: pts[0].pack_std_qty,
-              current_boxes: pts[0].boxes,
-              current_parts: pts[0].parts,
-              require_boxes: 1,
-              require_parts: 1,
-            });
-            super.showToast(this.toastCtrl, '已添加零件' + pts[0].part_name);
-            this.label = '';
+    else {
+      //合法标签
+      let loading = super.showLoading(this.loadingCtrl, '加载中...');
+      this.api.get('wm/getPartByLN', {
+        plant: this.item.plant,
+        workshop: this.item.workshop,
+        ln: this.label
+      }).subscribe((res: any) => {
+          if (res.successful) {
+            let pts = res.data;
+            if (pts.length > 0) {
+              this.item.parts.push({
+                plant: pts[0].plant,
+                workshop: pts[0].workshop,
+                part_no: pts[0].part_no,
+                part_name: pts[0].part_name,
+                supplier_id: pts[0].supplier_id,
+                supplier_name: pts[0].supplier_name,
+                dloc: pts[0].dloc,
+                unit: pts[0].unit,
+                std_qty: pts[0].pack_std_qty,
+                current_boxes: pts[0].boxes,
+                current_parts: pts[0].parts,
+                require_boxes: pts[0].require_boxes,
+                require_parts: pts[0].require_parts,
+              });
+              super.showToast(this.toastCtrl, '已添加零件' + pts[0].part_name);
+              this.label = '';
+            }
+          } else {
+            super.showToast(this.toastCtrl, res.message);
           }
-        } else {
-          super.showToast(this.toastCtrl, res.message);
-        }
-        loading.dismiss();
-        this.reload();
-      },
-      (error) => {
-        alert('系统错误,' + error);
-        loading.dismiss();
-        this.reload();
-      });
+          loading.dismiss();
+          this.reload();
+        },
+        (error) => {
+          alert('系统错误,' + error);
+          loading.dismiss();
+          this.reload();
+        });
+    }
   }
+
+  //手工调用，重新加载数据模型
+  refreshDataModal() {
+    setTimeout(() => {
+      this.label = '';
+      this.searchbar.setFocus();
+    }, 1000);
+  }
+
 
   //非标跳转Modal页
   changeQty(part) {
