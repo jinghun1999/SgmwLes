11aed129b09b97fc40e9c5927381d476dba325d0
jinghun1999 <minggang@outlook.com>
2021-03-15 10:47:56 +0800

    envs update / sound when scan reset

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -11,7 +11,7 @@ import { HttpErrorResponse } from '@angular/common/http'
 import { TimeoutError } from 'rxjs';
 import { Api, User } from "../../providers";
 import { HomePage, BaseUI } from "../";
-import { NativeAudio } from '@ionic-native/native-audio';
+// import { NativeAudio } from '@ionic-native/native-audio';
 @IonicPage()
 @Component({
     selector: "page-login",
@@ -21,7 +21,7 @@ export class LoginPage extends BaseUI {
     @ViewChild("userName") usernameInput;
     version: string;
     res: any = {};
-    environment: any[] = [];
+    envs: any[] = [];
     workshop: string;
     gender: string = "";
     account: { name: string; password: string } = {
@@ -36,59 +36,54 @@ export class LoginPage extends BaseUI {
         public toastCtrl: ToastController,
         public loadingCtrl: LoadingController,
         public alertCtrl: AlertController,
-        private nativeAudio: NativeAudio,
+        // private nativeAudio: NativeAudio,
         public api: Api
     ) {
         super();
         this.version = this.api.version;
-        //uniqueId1为音频文件的唯一ID
-        //assetPath音频资产的相对路径或绝对URL（包括http：//）
-        //官网还有更多的配置，这里只需要两个参数就行了，后面的回调记得带上
-        this.nativeAudio.preloadSimple('success', 'assets/audio/yes.wav').then(this.onSuccess, this.onError);
-        this.nativeAudio.preloadSimple('error', 'assets/audio/no.wav').then(this.onSuccess, this.onError);
+        // //uniqueId1为音频文件的唯一ID
+        // //assetPath音频资产的相对路径或绝对URL（包括http：//）
+        // //官网还有更多的配置，这里只需要两个参数就行了，后面的回调记得带上
+        // this.nativeAudio.preloadSimple('yes', 'assets/audio/yes.wav').then(this.onSuccess, this.onError);
+        // this.nativeAudio.preloadSimple('no', 'assets/audio/no.wav').then(this.onSuccess, this.onError);
     }
 
     ionViewDidLoad() {
         this.setFocus();
         //配置登录环境
-        this.environment = [
+        this.envs = [
             //生产环境不显示
-            //   {
-            //     id: 1,
-            //     text: "开发环境",
-            //     value: "http://127.0.0.1:49280",   
-            //   },      
+              {
+                id: 1,
+                text: "开发环境",
+                value: "http://localhost:49280",   
+              },
             {
                 id: 2,
-                text: "冲压测试环境",
-                value: "http://10.1.126.171/CYWEBAPI",
+                text: "生产环境",
+                value: "https://hxlesapp.sgmw.com.cn",
             },
             {
                 id: 3,
-                text: "厂内环境",
+                text: "测试环境",
                 value: "http://10.1.126.171/lesapi",
-            },
-            {
-                id: 4,
-                text: "厂外环境",
-                value: "http://222.84.126.54:8081",
             }
         ];
         const env = localStorage.getItem('les_env');
-        if (this.environment.findIndex(e => e.value == env) === -1) {
-            this.api.api_host = this.environment[1].value;
+        if (env && this.envs.findIndex(e => e.value == env) > -1) {
+            this.api.api_host = env;
         }
         else {
-            this.api.api_host = env;
+            this.api.api_host = this.envs[0].value;
         }
     }
     onSuccess() { }
     onError() { }
     doLogin() {
-        // play yes audio when suceessed.
-        this.nativeAudio.play('ok').then(this.onSuccess, this.onError);
-        // play no audio when failed.
-        this.nativeAudio.play('no').then(this.onSuccess, this.onError);
+        // // play yes audio when suceessed.
+        // this.nativeAudio.play('yes').then(this.onSuccess, this.onError);
+        // // play no audio when failed.
+        // this.nativeAudio.play('no').then(this.onSuccess, this.onError);
         if (!this.account.name || !this.account.password) {
             super.showToast(this.toastCtrl, "请输入用户名密码");
             this.setFocus();
